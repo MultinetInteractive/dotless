bb5bf10921cf867f453c79b58201c542c24b8b6a
Anders Mörner <anmo@multinet.se>
2024-10-04 16:34:25 +0200

    simplify

--- src/dotless.Core/Parser/Tree/Ruleset.cs
+++ src/dotless.Core/Parser/Tree/Ruleset.cs
@@ -494,7 +494,7 @@ namespace dotless.Core.Parser.Tree
 
                         env.Output.Append(env.Compress ? "{" : " {\n  ");
 
-                        env.Output.AppendMany(rules.ConvertAll(stringBuilder => stringBuilder.ToString()).Distinct(),
+                        env.Output.AppendMany(rules,
                             env.Compress ? "".AsMemory() : "\n  ".AsMemory());
 
                         if (env.Compress) {
